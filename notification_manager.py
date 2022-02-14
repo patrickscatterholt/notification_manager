05de5c88ef3a04ef1b915e88bd37ed16b7d1f91c
Patrick <patrick@scatterholt.com>
2022-02-14 15:57:34 -0500

    Add Twilio settings

--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,9 @@
+from twilio.rest import Client
+
+TWILIO_SID = "AC592fdffd6f2a6a7d87a36b10103eff2a"
+TWILIO_AUTH_TOKEN = "a3c80c98e0a7675fffb61d2ff431ce58"
+
+
 class NotificationManager:
 
     def __init__(self):
